3503d5b03447ed8cc86c5e8d345be2e259507c96
Subarna1911 <subarnablone09@gmail.com>
2023-02-12 18:07:44 +0530

    package added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       
-        <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar toggleable="lg" type="dark" variant="success">
           <b-container>
           <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
@@ -10,8 +10,9 @@
 
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item href="#">Link</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
+              <b-nav-item href="#">About us</b-nav-item>
+              <b-nav-item href="#">Contact us</b-nav-item>
+              
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
@@ -22,12 +23,12 @@
                   class="mr-sm-2"
                   placeholder="Search"
                 ></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                <b-button variant="dark" size="sm" class="my-2 my-sm-0" type="submit"
                   >Search</b-button
                 >
               </b-nav-form>
 
-              <b-nav-item-dropdown text="Lang" right>
+              <b-nav-item-dropdown text="services" right>
                 <b-dropdown-item href="#">EN</b-dropdown-item>
                 <b-dropdown-item href="#">ES</b-dropdown-item>
                 <b-dropdown-item href="#">RU</b-dropdown-item>
@@ -37,7 +38,7 @@
               <b-nav-item-dropdown right>
                 <!-- Using 'button-content' slot -->
                 <template #button-content>
-                  <em>User</em>
+                  <em>User login</em>
                 </template>
                 <b-dropdown-item href="#">Profile</b-dropdown-item>
                 <b-dropdown-item href="#">Sign Out</b-dropdown-item>
@@ -47,12 +48,20 @@
         </b-container>
         </b-navbar>
 
-       <b-container class="mt-5">
-          <b-button variant="primary" size="lg" class="rounded-0">Button</b-button>
-       </b-container>
-     
+        <b-button variant="outline-info" class="mt-4 mx-4">Button</b-button>
+        
     </div>
+<div>
+  <b-card  style="max-width: 200px;" class="mt-4 mx-3 img-top text-center" img-src="https://picsum.photos/600/300/?image=25" img-alt="image">
+  <b-card-title>My card</b-card-title>
+  <b-card-text>this is my first card ever in vuejs and bootstrap vue</b-card-text>
+  <b-button  variant="success">Explore here</b-button>
+  </b-card>
+</div>
+    
   </div>
+ 
+  
 </template>
 
 <script>
