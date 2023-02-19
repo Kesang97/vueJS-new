5c412cf74eabe7c96d29d78bce544df31f7695e8
Subarna1911 <subarnablone09@gmail.com>
2023-02-19 18:46:05 +0530

    dashboard

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -17,10 +17,11 @@
                             <div class="input-group-text bg-transparent">
                                 <i class="fas fa-search"></i>
                             </div>
+                           
                         </span>
+                      
                     </div>
-                    
-                        
+              
                     </li>
                 </ul>
                 
@@ -37,6 +38,40 @@
             <div class="img text-center px-auto">
                 <img src="../assets/side_logo.png" class="img-fluid" width="120px" alt="">
             </div>
+            
+
+            <div>
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Dashboard" left>
+        <b-dropdown-item>Food</b-dropdown-item>
+        
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="Menus" left>
+        <b-dropdown-item href="#">Menu Catalogue</b-dropdown-item>
+        <b-dropdown-item href="#">Menu List</b-dropdown-item>
+        <b-dropdown-item href="#">Menu Grid</b-dropdown-item>
+        <b-dropdown-item href="#">Add product</b-dropdown-item>
+        <b-dropdown-item href="#">Product detail</b-dropdown-item>
+         </b-nav-item-dropdown>
+         <b-nav-item href="#">Orders</b-nav-item>
+         <b-nav-item href="#">Restaurants List</b-nav-item>
+         <b-nav-item-dropdown text="Customers" left>
+        <b-dropdown-item href="#">Customers List</b-dropdown-item>
+        <b-dropdown-item href="#">Customers Review</b-dropdown-item>
+        <b-dropdown-item href="#">Social Activity</b-dropdown-item>
+    </b-nav-item-dropdown>
+
+    <b-nav-item href="#">Sales</b-nav-item>
+   
+    </b-navbar-nav>
+
+</div>
+
         </div>
     </b-sidebar>
 
